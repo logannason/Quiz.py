42866f38d8720370cf844a722dd7c12b7f7fa468
acuevas123 <antonio.cuevas@innovationcharter.org>
2015-10-26 12:21:39 -0400

    Antonio's code

---
+++ quizgame.py
@@ -0,0 +1,18 @@
+print("Super Personality Quizarino")
+name = raw_input("Do you actually listen to rap or r&b?, Yes or No?")
+     if "Yes" print("Welcome to Super Personality Quizarino c:")
+     if "No" print ("Well this is gonna be a long ride then...")
+name = raw_input("What is your favorite rapper, Eminem, Kendrick Lamar, Tupac, The Weeknd, P reign, or Jhene Aiko")
+     if "Eminem" print("Good choice, time for the next question")
+     if "Kendrick Lamar" print("Interesting, time for the next question")
+     if "Tupac" print("A legend, time for the next question")
+     if "The Weeknd" print("A rising star, time for the next question")
+name = raw_input("What era of rap do you enjoy the most? Eighties, Ninties, Early two Thousanads, Mordern Artsists?")
+     if "Eighties" print("Most people would agree with you, on to the next question.")
+     if "Ninties" print("A very popular option, on to the next question.")
+     if "Early two Thousands" print("Not exactly the best time period of the genre, but you can think what you'd like. on to the next question.")
+     if "Modern Artists" print("There definetly are some shining gems in todays industry, on to the next question.")
+name = raw_input("Do you prefer Commercial Artists such as Drake, Lil Wayne, or Kendrick Lamar or Underground Preformers such as Geto Boys, Brother Ali, or Mick Jenkins?"
+                 if "Commercial Artists" print(" Not that surprising in the slightest, but a boring answer none the less. Let's move on to the next question.")
+                 if "Underground Preformers" print("A very broad answer, but respectable. Let's move on to the next question.")
+name = raw_input(
