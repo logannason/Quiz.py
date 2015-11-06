51edf046118cf46859e20c52b46dcd745c107a13
acuevas123 <antonio.cuevas@innovationcharter.org>
2015-11-06 09:14:38 -0500

    k

--- quizgame.py
+++ quizgame.py
@@ -4,16 +4,16 @@ def Title_Page():
 #antonio wrote the questions
 #logan made functions and if statements
 def Ask_questions(Questions,):
-    name = raw_input("Do you actually listen to rap or r&b?, Yes or No?")
+    red = raw_input("Do you actually listen to rap or r&b?, Yes or No?")
     if "Yes":
          print("Welcome to Super Personality Quizarino")
     if "No":
          print ("Well this is gonna be a long ride then...")
-    Jimmy = raw_input("What is your favorite rapper, Eminem, Kendrick Lamar, Tupac, Wiz Khalifa, Drake, or A$AP Mob")
+    blue = raw_input("What is your favorite rapper, Eminem, Kendrick Lamar, Tupac, Wiz Khalifa, Drake, or A$AP Mob")
     if "Eminem":
         print("The God of the Game, time for the next question")
     if "Kendrick Lamar":
-        print("Trash, time for the next question")
+        print("A God, time for the next question")
     if "Tupac":
         print("A legend, time for the next question")
     if "Wiz Khalifa":
@@ -22,7 +22,7 @@ def Ask_questions(Questions,):
         print("Hold on were going to the next question")
     if "A$AP Mob":
         print("I got hella more questions") 
-    Timmy = raw_input("What era of rap do you enjoy the most? 80's, 90's, 2000's, 2010's?")
+    green = raw_input("What era of rap do you enjoy the most? 80's, 90's, 2000's, 2010's?")
     if "80":
         print("Most people would agree with you, on to the next question.")
     if "90":
@@ -31,16 +31,26 @@ def Ask_questions(Questions,):
         print("Not exactly the best time period of the genre, but you can think what you'd like. on to the next question.")
     if "2010":
         print("There definetly are some shining gems in todays industry, on to the next question.")
-    MOM = raw_input("Do you prefer Commercial Artists or Underground Preformers?")
+    white = raw_input("Do you prefer Commercial Artists or Underground Preformers?")
     if "Commercial Artist":
         print(" Not that surprising in the slightest, but a boring answer none the less. Let's move on to the next question.")
     if "Underground Preformers":
         print("A very broad answer, but respectable. Let's move on to the next question.")
 
-    Hi = raw_input("Who do you prefer G-Eazy or Tech n9ne")
+    black = raw_input("Who do you prefer G-Eazy or Tech n9ne")
     if "G-Eazy":
         print("A laid back fellow I see")
     if "Tech n9ne":
-        print("You are Insane in the Membrane")
+        print("You are a little Dysfunctional")
+    pink = raw_input("Who do you like more, Kendrick Lamar or Kendirck Lamar?")
+    if "Kendrick Lamar":
+        print("There really isn't any other option, is there?")
+    purple = raw_input("Who do you think has a better performance personality, A$AP Rocky or Tyler the Creator?")
+    if "A$AP Rocky"
+        print("Lord Pretty Flacko the 2nd approves.")
+    if "Tyler the Creator"
+        print("Look at that you earned the Wolf Gang's favor.")
+    yellow = raw_input("
         #Logan wrote this
+        #not really
 
